c868f3e3a4ca81b4ea4fbe96ae8ca7be336d2915
rnsbrz <brazanas@gmail.com>
2024-05-02 21:56:33 +0300

    added Hello World

--- src/main/java/com/spring/calculator/CalculatorApplication.java
+++ src/main/java/com/spring/calculator/CalculatorApplication.java
@@ -2,12 +2,24 @@ package com.spring.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+// @SpringBootApplication anotacija yra lygi @Configuration, @EnableAutoConfiguration ir @ComponentScan
+//nurodoma anotacija klaseja, turincioje pagrindini (main) metoda
 @SpringBootApplication
-public class CalculatorApplication {
+// web kontroleris. pazymi mvc valdikli. leidzia viduja naudoti @requestMapping
+// @RestController anotacija nurodo, jog String tipo rezultatas turetu buti isspausdinamas klientui toks koks yra.
+@RestController
 
+public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+		System.out.println("Woohoo pirmoji spring boot aplikacija");
 	}
-
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
+	return String.format("Hello %s!", name);
+	}
 }
